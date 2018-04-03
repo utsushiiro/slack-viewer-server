ab9870758fae477d030bfabcbc92dcf3fbd8d052
utsushiiro <ozeki.haru@gmail.com>
2018-04-03 19:16:37 +0900

    eslint --fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  const param = {"ok": "false"};
+  const param = { ok: 'false' };
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(err.status || 500);
   res.send(param);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const param = {"ok": "true", "value": "sample"};
+  const param = { ok: 'true', value: 'sample' };
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.send(param);
 });
